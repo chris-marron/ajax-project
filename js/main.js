1b4e1137c72fc7e9aeffaf1b20b5bd24d0c59802
Chris Marron <chrisluk04@gmail.com>
2023-01-18 04:58:12 +0000

    filter or submit

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
 var $ulManga = document.querySelector('.all-manga');
 var $searchInput = document.querySelector('.input');
+// var $searchButton = document.querySelector('.search-button');
 var clearButton = document.querySelector('.clear-search');
-
-function apiList(result) {
+var $form = document.querySelector('form');
+function apiList() {
+  var value = $searchInput.value.toLowerCase();
+  // console.log(value);
 
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.jikan.moe/v4/top/manga?q=$' + result);
+  xhr.open('GET', 'https://api.jikan.moe/v4/top/manga?q=$' + value);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function (e) {
-    var dataObj = xhr.response.data;
-    domReload(dataObj);
+    var apiObj = xhr.response.data;
+    dataLoop(apiObj);
+    // var sack = document.querySelectorAll('li');
+    // for (var i = 0; i < sack.length; i++) {
+    //   var sackH3 = sack[i].querySelectorAll('h3')[0];
+    //   var txt = sackH3.textContent;
+    //   console.log(sackH3);
+    //   if (txt.toLowerCase().indexOf(value) > -1) {
+    //     sack[i].setAttribute('class', 'go');
+
+    //   } else {
+    //     sack[i].remove();
+    //   }
+    // }
+    // renderTree(value, apiObj);
   });
 
   xhr.send();
-}
-
-function oneTwo(e) {
-  var value = e.target.value;
-  if (value.length > 0) {
-    // console.log(value);
-    apiList(value);
-  }
 
 }
-$searchInput.addEventListener('input', oneTwo);
+// $searchInput.addEventListener('input', apiList);
 
-function renderTree(result) {
-  var $li = document.createElement('li');
-  $li.classList.add('start-left');
-  var $img = document.createElement('img');
-  $img.setAttribute('src', result.images.jpg.image_url);
-  $li.prepend($img);
-  var $title = document.createElement('h3');
-  $title.textContent = result.title;
-  $li.appendChild($title);
-  $ulManga.appendChild($li);
+$form.addEventListener('submit', function (eve) {
+  eve.preventDefault();
 
-  return $li;
-}
+  $searchInput.addEventListener('input', apiList);
+});
 
 function clearList(ok) {
   while ($ulManga.firstChild) {
@@ -47,17 +47,37 @@ function clearList(ok) {
 }
 clearButton.addEventListener('click', clearList);
 
-function domReload(data) {
+// function renderTree(value, obj) {
+//   for (var q = 0; q < obj.length; q++) {
+//     if (value && value.trim() === obj[q].title.toLowerCase()) {
+//       var $li = document.createElement('li');
+//       var newTitle = document.createElement('h3');
+//       newTitle.textContent = obj[q].title;
+//       $li.appendChild(newTitle);
+//       var newImg = document.createElement('img');
+//       newImg.setAttribute('src', obj[q].images.jpg.image_url);
+//       $li.prepend(newImg);
+//       $ulManga.appendChild($li);
+//     }
+//   }
+// }
+function renderAll(result) {
+  var $li = document.createElement('li');
+  var newTitle = document.createElement('h3');
+  newTitle.textContent = result.title;
+  $li.appendChild(newTitle);
+  var newImg = document.createElement('img');
+  newImg.setAttribute('src', result.images.jpg.image_url);
+  $li.prepend(newImg);
+  $ulManga.appendChild($li);
+
+  return $ulManga;
+}
+function dataLoop(data) {
   for (var i = 0; i < data.length; i++) {
-
-    var wow = data[i].title;
-    if (wow) {
-      renderTree(data[i]);
-    } // else {
-
-    // }
-
+    var wow = renderAll(data[i]);
   }
   return wow;
 }
-document.addEventListener('DOMContentLoaded', domReload);
+
+apiList();
